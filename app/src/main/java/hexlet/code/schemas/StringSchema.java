8217132d18c334a6115ebaf91590c92b58d42365
Aivaseda <aivaseda@bk.ru>
2024-08-07 11:30:09 +0500

    2 point

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,9 +2,10 @@ package hexlet.code.schemas;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
-import static java.awt.SystemColor.text;
 import static java.util.Objects.isNull;
 
 public class StringSchema {
@@ -17,12 +18,10 @@ public class StringSchema {
         requiredPredicate = value -> !isNull(value) && !value.isEmpty();
         return this;
     }
-
     public StringSchema minLength(int length) {
         minLengthPredicate = value -> value.length() >= length;
         return this;
     }
-
     public StringSchema contains(String string){
         subString = string;
         containsPredicate = value -> value.contains(subString);
@@ -30,41 +29,21 @@ public class StringSchema {
     }
 
     public boolean isValid(String value) {
-        boolean switcher = true;
-        if (requiredPredicate == null /* || value.equals("") && requiredPredicate == null */) {
-            switcher = true;
-        }else if (requiredPredicate != null /*|| value.equals("") && requiredPredicate != null8*/) {
-            return requiredPredicate.test(value);
-        }
+        List<Boolean> statusList = new ArrayList<>();
+
         if (minLengthPredicate != null) {
-            switcher = minLengthPredicate.test(value);
+            statusList.add(minLengthPredicate.test(value));
+        }
+        if (subString != null) {
+            statusList.add(containsPredicate.test(value));
         }
 
-        if (containsPredicate != null) {
-            switcher = containsPredicate.test(value);
+
+        if (requiredPredicate == null) {
+            statusList.add(true);
+        }else {
+            statusList.add(requiredPredicate.test(value));
         }
-        return switcher;
+        return !statusList.contains(false);
     }
 }
-
-
-
-//private String data;
-//private int length;
-//private String subString;
-//
-//
-//
-//public boolean required(){
-//    return
-//}
-//public boolean minLength(int length){
-//    return value -> length <= value.length()ж
-//}
-//public boolean contains(String subString){
-//    this.subString = subString;
-//}
-//
-//public boolean isValid(String o) {
-//    return false;
-//}
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -9,9 +9,21 @@ public final class AppTest {
     public void firstTest() {
         var v = new Validator();
         var schema = v.string();
+        assertThat(schema.isValid("")).isTrue();
+        assertThat(schema.isValid(null)).isTrue();
         schema.required();
-        //assertThat(schema.isValid("")).isTrue();
-//        assertThat(schema.minLength(4).contains("wh3453453").isValid("what does the fox say")).isTrue();
-        assertThat(schema.minLength(4).isValid("")).isFalse();
+        assertThat(schema.isValid("")).isFalse();
+        assertThat(schema.isValid(null)).isFalse();
+
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("wh").isValid("whwhwhwhwhwhwh")).isTrue();
+        assertThat(schema.contains("000000").isValid("what does the fox say")).isFalse();
+
+        assertThat(schema.minLength(10).minLength(4).isValid("Hexlet")).isFalse();
+        assertThat(schema.minLength(15).isValid("Hexlet")).isFalse();
+
+        assertThat(schema.contains("ex").minLength(4).isValid("Hexlet")).isTrue();
+
+
     }
 }
