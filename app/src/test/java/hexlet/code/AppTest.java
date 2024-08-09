4a8eaab69906c7d16b63050860aff122d650f2f4
Aivaseda <aivaseda@bk.ru>
2024-08-09 07:11:39 +0500

    uncommit

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -5,25 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class AppTest {
-//    @Test
-//    public void stringTest() {
-//        var v = new Validator();
-//        var schema = v.string();
-//        assertThat(schema.isValid("")).isTrue();
-//        assertThat(schema.isValid(null)).isTrue();
-//        schema.required();
-//        assertThat(schema.isValid("")).isFalse();
-//        assertThat(schema.isValid(null)).isFalse();
-//
-//        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
-//        assertThat(schema.contains("wh").isValid("whwhwhwhwhwhwh")).isTrue();
-//        assertThat(schema.contains("000000").isValid("what does the fox say")).isFalse();
-//
-//        assertThat(schema.minLength(10).minLength(4).isValid("Hexlet")).isFalse();
-//        assertThat(schema.minLength(15).isValid("Hexlet")).isFalse();
-//
-//        assertThat(schema.contains("ex").minLength(4).isValid("Hexlet")).isTrue();
-//    }
+    @Test
+    public void stringTest() {
+        var v = new Validator();
+        var schema = v.string();
+        assertThat(schema.isValid("")).isTrue();
+        assertThat(schema.isValid(null)).isTrue();
+        schema.required();
+        assertThat(schema.isValid("")).isFalse();
+        assertThat(schema.isValid(null)).isFalse();
+
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("wh").isValid("whwhwhwhwhwhwh")).isTrue();
+        assertThat(schema.contains("000000").isValid("what does the fox say")).isFalse();
+
+        assertThat(schema.minLength(10).minLength(4).isValid("Hexlet")).isFalse();
+        assertThat(schema.minLength(15).isValid("Hexlet")).isFalse();
+
+        assertThat(schema.contains("ex").minLength(4).isValid("Hexlet")).isTrue();
+    }
 
     @Test
     public void numberTest() {
